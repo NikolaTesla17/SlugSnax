22120a376e1f221ae39716bf1eb6ccb3de3ce86c
Nayan Smuek <44534701+NikolaTesla17@users.noreply.github.com>
2023-02-08 02:31:58 -0800

    more functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 fetch('static/foodScores.json').then(response => response.json()).then(function(foodScores) {
 fetch('https://ucsc.cc/api', {method: 'GET'}).then(function(response) { return response.json(); }).then(function(json) {
     diningHalls = {};
+    maxFoodScore = 0;
+    bestFood = "Error Finding Food";
     for (var i = 0; i < json[0].halls.length; i++) {
         if (json[0].halls[i].name == "Porter/Kresge") {
             i++;
@@ -12,6 +14,10 @@ fetch('https://ucsc.cc/api', {method: 'GET'}).then(function(response) { return r
                 for (var x = 0; x < json[0].halls[i].meals[j].cats[k].foods.length; x++){
                     for (var y = 0;y < foodScores.length; y++){
                         if(foodScores[y].name == json[0].halls[i].meals[j].cats[k].foods[x].name){
+                            if(foodScores[y].points>maxFoodScore){
+                                maxFoodScore = foodScores[y].points;
+                                bestFood = foodScores[y].name;
+                            }
                             diningHalls[json[0].halls[i].name] += foodScores[y].points;
                         }
                     }
@@ -19,9 +25,17 @@ fetch('https://ucsc.cc/api', {method: 'GET'}).then(function(response) { return r
             }
         }
     }
-    switch (Object.keys(diningHalls).reduce(function(a, b){ return diningHalls[a] > diningHalls[b] ? a : b })){
-        case "Cowell/Stevenson":
-            document.getElementById("first").innerHTML = "Cowell/Stevenson" + document.getElementById("first").innerHTML;
-    }
+    document.getElementById("podiumTop").innerHTML = Object.keys(diningHalls).reduce(function(a, b){ return diningHalls[a] > diningHalls[b] ? a : b }) + document.getElementById("podiumTop").innerHTML;
+    document.getElementById("topPoints").innerHTML = diningHalls[Object.keys(diningHalls).reduce(function(a, b){ return diningHalls[a] > diningHalls[b] ? a : b })] + document.getElementById("topPoints").innerHTML;
+    document.getElementById("bestFood").innerHTML = bestFood;
+    delete diningHalls[Object.keys(diningHalls).reduce(function(a, b){ return diningHalls[a] > diningHalls[b] ? a : b })];
+    mid = Object.keys(diningHalls).reduce(function(a, b){ return diningHalls[a] > diningHalls[b] ? a : b }).split("/")[0];
+    if (mid == "College Nine"){mid = "College 9"}
+    document.getElementById("podiumMid").innerHTML = mid;
+
+    delete diningHalls[Object.keys(diningHalls).reduce(function(a, b){ return diningHalls[a] > diningHalls[b] ? a : b })];
+    participant = Object.keys(diningHalls).reduce(function(a, b){ return diningHalls[a] > diningHalls[b] ? a : b }).split("/")[0];
+    if (participant == "College Nine"){participant = "College 9"}
+    document.getElementById("participant").innerHTML = participant;
 });
 });
