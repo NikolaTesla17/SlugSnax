d09ebc8a8b67da683b41427653786377cbc750f1
Nayan Smuek <44534701+NikolaTesla17@users.noreply.github.com>
2023-02-08 06:50:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ fetch('https://ucsc.cc/api', {method: 'GET'}).then(function(response) { return r
             backgroundColorScale = "Breakfast";
                 j = 0;
                 if(7-1-hours==0){
+                    document.getElementById("timeTillMealChange").innerHTML = "Breakfast - In " + (7-hours) + " hours and " + (60-minutes) + " minutes";
                     if (minutes == 0){
                         document.getElementById("timeTillMealChange").innerHTML = "Breakfast - In 1 hour";
                     } else if (minutes=60){
@@ -91,7 +92,6 @@ fetch('https://ucsc.cc/api', {method: 'GET'}).then(function(response) { return r
                     }
                     document.getElementById("timeTillMealChange").innerHTML = "Breakfast - In " + (60-minutes) + " minutes";
                 }
-                document.getElementById("timeTillMealChange").innerHTML = "Breakfast - In " + (7-hours) + " hours and " + (60-minutes) + " minutes";
             }
             else if(hours>=7&&11>hours){//breakfast
                 j = 0;
